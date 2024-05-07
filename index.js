14f28423ac3038f4f7944b84829bc29b11b62020
*Lungile* <*lungile.simelane4@gmail.com*>
2024-05-07 02:40:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -56,30 +56,41 @@ function search(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
-  console.log(apiUrl);
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml += `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml += `
             <div class="weather-forecast-day">
-              <div class="weather-forecast-date">${day}</div>
-              <div class="weather-forecast-icon">⛅</div>
+              <div class="weather-forecast-date">${formatDay(day.time)}</div>
+              <div class="weather-forecast-icon">
+              <img src="${day.condition.icon_url}"/>
+              </div>
               <div class="weather-forecast-temp">
-                <span class="weather-forecast-max-temp"><strong>18°</strong></span>
-                <span class="weather-forecast-min-temp">12°</span>
+                <span class="weather-forecast-max-temp"><strong>${Math.round(
+                  day.temperature.maximum
+                )}°</strong></span>
+                <span class="weather-forecast-min-temp">${Math.round(
+                  day.temperature.minimum
+                )}°</span>
               </div>
             </div>
             `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
